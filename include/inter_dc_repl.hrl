707a6739e982398f8e3115a0cf80a367c14e95fe
System Administrator <mars.leezm@gmail.com>
2015-04-08 22:00:04 +0100

    Add metadata

--- include/inter_dc_repl.hrl
+++ include/inter_dc_repl.hrl
@@ -4,4 +4,7 @@
          %%Track timestamps from other DC which have been committed by this DC
          recQ :: orddict:orddict(), %% Holds recieving updates from each DC separately in causal order.
          statestore,
+         pending_ver :: orddict:orddict(), %%Pending versions that are not yet visible to local DC
+         stale_sum :: integer(), %%The sum of staleness count
+         processed_num :: non_neg_integer(), %%The number of object versions processed and visible to local DC 
          partition}).
