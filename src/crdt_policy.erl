ef71bcda0e945d3d6f610939336a5e9f15d2310d
Mathias Weber <mathias@weber-inter.net>
2016-01-22 17:42:07 +0100

    policy crdt

---
+++ src/crdt_policy.erl
@@ -0,0 +1,145 @@
+%% -------------------------------------------------------------------
+%%
+%% crdt_policy: A convergent, replicated, operation based security policy implementation
+%%
+%% This file is provided to you under the Apache License,
+%% Version 2.0 (the "License"); you may not use this file
+%% except in compliance with the License.  You may obtain
+%% a copy of the License at
+%%
+%%   http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing,
+%% software distributed under the License is distributed on an
+%% "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+%% KIND, either express or implied.  See the License for the
+%% specific language governing permissions and limitations
+%% under the License.
+%%
+%% -------------------------------------------------------------------
+
+%% @doc
+%% An operation-based Security Policy implementation
+%% As the data structure is operation-based, to issue an operation, one should
+%% firstly call `generate_downstream/3' to get the downstream version of the
+%% operation and then call `update/2'.
+%%
+%% It provides ??? operations: TODO
+%%
+%% @end
+-module(crdt_policy).
+
+%% API
+-export([new/0, value/1, generate_downstream/3, update/2, equal/2,
+    to_binary/1, from_binary/1, value/2, is_operation/1]).
+
+-ifdef(TEST).
+-include_lib("eunit/include/eunit.hrl").
+-endif.
+
+-export_type([policy/0, right/0, binary_policy/0, policy_op/0, policy_downstream_op/0]).
+-opaque policy() :: orddict:orddict().
+
+-type binary_policy() :: binary(). % A binary that from_binary/1 will operation on
+
+-type policy_op() :: {set_right, right()}.
+
+-opaque policy_downstream_op() :: {set_right, right(), token(), orddict:orddict()}.
+
+-type right() :: ordsets:ordset(term()).
+-type token() :: term().
+
+-type actor() :: riak_dt:actor().
+
+-spec new() -> policy().
+new() ->
+    orddict:new().
+
+
+-spec value(policy()) -> [right()].
+value(Policy) ->
+    orddict:fold(fun(Right, Tokens, Res)->
+                  lists:duplicate(length(Tokens), Right) ++ Res end,
+                 [], Policy).
+
+-spec value(get_right, policy()) -> right().
+value(get_right, Policy) ->
+    case orddict:fetch_keys(Policy) of
+        [] ->
+            ordsets:new();
+        [Head|Tail] ->
+            lists:foldl(fun ordsets:intersection/2, Head, Tail)
+    end.
+
+-spec generate_downstream(policy_op(), actor(), policy()) -> {ok, policy_downstream_op()}.
+generate_downstream({set_right, Right}, Actor, CurrentPolicy) ->
+    {ok, {set_right, Right, unique(Actor), CurrentPolicy}}.
+
+-spec update(policy_downstream_op(), policy()) -> {ok, policy()}.
+update({set_right, Right, Token, Dependencies}, Policy) ->
+    add_right(Right, Token, remove_rights(Dependencies, Policy)).
+
+-spec equal(policy(), policy()) -> boolean().
+equal(Policy1, Policy2) ->
+    Policy1 == Policy2. % Should work if comparing orddicts works
+
+
+%-include_lib("riak_dt/include/riak_dt_tags.hrl").
+-define(TAG, 99).
+-define(V1_VERS, 1).
+
+-spec to_binary(policy()) -> binary_policy().
+to_binary(Policy) ->
+    %% @TODO somthing smarter
+    <<?TAG:8/integer, ?V1_VERS:8/integer, (term_to_binary(Policy))/binary>>.
+
+-spec from_binary(binary_policy()) -> {ok, policy()}.
+from_binary(<<?TAG:8/integer, ?V1_VERS:8/integer, Bin/binary>>) ->
+    %% @TODO somthing smarter
+    binary_to_term(Bin).
+
+-spec is_operation(term()) -> boolean().
+is_operation(Operation) ->
+    case Operation of
+        {set_right, _Right, _Token, _Dependencies} ->
+            true;
+        _ ->
+            false
+    end.
+
+
+% Private
+add_right(Right, Token, Policy) ->
+    case orddict:find(Right, Policy) of
+        {ok, Tokens} ->
+            case lists:member(Token, Tokens) of
+                true ->
+                    Policy;
+                false ->
+                    orddict:store(Right, Tokens++[Token], Policy)
+            end;
+        error ->
+            orddict:store(Right, [Token], Policy)
+    end.
+
+remove_right(Right, RemoveTokens, Policy) ->
+    case orddict:find(Right, Policy) of
+        {ok, Tokens} ->
+            RestTokens = Tokens--RemoveTokens,
+            case RestTokens of
+                [] ->
+                    orddict:erase(Right, Policy);
+                _ ->
+                    orddict:store(Right, RestTokens, Policy)
+            end;
+        error ->
+            Policy % if the right is not in the policy anymore, it was already remove by a different policy change
+    end.
+
+remove_rights(Orddict, Policy) ->
+    orddict:fold(fun(Right, Tokens, Res) ->
+                   remove_right(Right, Tokens, Res)
+                 end, Policy, Orddict).
+
+unique(_Actor) ->
+    crypto:strong_rand_bytes(20).
